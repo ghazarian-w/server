df68fdd4da5a3e4ad8fc92846052f2d13f535735
William Ghazarian <ghazarian.w@proton.me>
2024-08-15 07:45:56 +0200

    Adding base files to the API layer

---
+++ DevOpsInventoryManagement.API/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DevOpsInventoryManagement.API.Mappers;
+using DevOpsInventoryManagement.API.Models;
+using DevOpsInventoryManagement.BLL.Interfaces;
+using DevOpsInventoryManagement.Domain.Entities;
+
+namespace DevOpsInventoryManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<User> register(UserRegisterFormDTO registerFormDTO)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _userService.Register(registerFormDTO.ToEntity());
+                return Created();
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<User> login(UserLoginFormDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(_userService.LoginToken(loginDTO.Login, loginDTO.Password));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ DevOpsInventoryManagement.API/Models/UserSessionDTO.cs
@@ -0,0 +1,9 @@
+namespace DevOpsInventoryManagement.Models
+{
+    public class UserSessionDTO
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = null!;
+    }
+}
